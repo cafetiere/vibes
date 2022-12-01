3bc209901b27b5f215af21301ff1c2c9970c29c0
svurrakula <svurrakula@gocardless.com>
2022-12-01 15:19:25 +0000

    use htmlparser instead to parse response'

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const rp = require('promise-request-retry');
-const jsdom = require('jsdom');
+const htmlparser = require("htmlparser2");
 
 const API_KEY = process.env.API_KEY
 const URL = 'http://quotes.toscrape.com/page/1/'
@@ -15,7 +15,9 @@ options = {
 
 rp(options)
     .then(response => {
-        console.log(response.body)
+        // console.log(response.body)
+        const dom = htmlparser.parseDocument(response)
+        console.log(dom)
     })
     .catch(error => {
         console.log(error)
