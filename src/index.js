f7ed4c3bb9f05a189f5876c8c652fdc2846f81f2
cafetiere <asuresh@gocardless.com>
2022-12-01 14:09:42 +0000

    created basic script

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const rp = require('promise-request-retry');
+
+const API_KEY = process.env.API_KEY
+const URL = 'http://quotes.toscrape.com/page/1/'
+
+options = {
+    uri: `http://api.scraperapi.com/`,
+    qs: {
+        'api_key': API_KEY,
+        'url': URL
+    },
+    resolveWithFullResponse: true
+}
+
+rp(options)
+    .then(response => {
+        console.log(response.body)
+    })
+    .catch(error => {
+        console.log(error)
+    })
