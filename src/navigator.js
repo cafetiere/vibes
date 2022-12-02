cdf3ccc85c50ad2fea285755e75cd7f5e07dd756
cafetiere <asuresh@gocardless.com>
2022-12-02 14:55:36 +0000

    add logic to get elements from css class

--- src/navigator.js
+++ src/navigator.js
@@ -8,9 +8,11 @@ function navigate_eater() {
     const pageNumbers = _.range(1, 11)
     pageNumbers.forEach((pageNumber) => {
         const url = `${base_url}/${pageNumber}`
-        console.log(url)
-        nightmare.goto(url)
-        //rest of code here 
+        const page = nightmare.goto(url)
+
+        const elements = page.evaluate(()=>{
+            return document.getElementsByClassName("c-compact-river__entry c-compact-river__entry--featured")
+        })
     })
 }
 
